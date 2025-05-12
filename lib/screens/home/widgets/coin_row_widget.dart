637ddd581d63ac064f345cfe9f9815c01728ba7d
Andrushazzz <miraiii99gg@yandex.ru>
2025-05-12 20:40:01 +0700

    feat: feat coin screen

--- lib/screens/home/widgets/coin_row_widget.dart
+++ lib/screens/home/widgets/coin_row_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../../../res/theme.dart';
+import '../coin_screen/coin_screen.dart';
 
 class CryptoRow extends StatelessWidget {
   final String name;
@@ -17,41 +18,48 @@ class CryptoRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(name, style: ProjectTextStyles.h2),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Row(
-                children: [
-                  Text.rich(
-                    TextSpan(
-                      style: ProjectTextStyles.h2,
-                      children: [
-                        TextSpan(
-                          text: price.split('.')[0],
-                          style: const TextStyle(color: ProjectColors.black),
-                        ),
-                        TextSpan(
-                          text: '.${price.split('.')[1]}\$',
-                          style: TextStyle(color: ProjectColors.grey3),
-                        ),
-                      ],
+    return InkWell(
+      onTap: () {
+        Navigator.of(
+          context,
+        ).push(MaterialPageRoute(builder: (context) => const CoinScreen()));
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(name, style: ProjectTextStyles.h2),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Row(
+                  children: [
+                    Text.rich(
+                      TextSpan(
+                        style: ProjectTextStyles.h2,
+                        children: [
+                          TextSpan(
+                            text: price.split('.')[0],
+                            style: const TextStyle(color: ProjectColors.black),
+                          ),
+                          TextSpan(
+                            text: '.${price.split('.')[1]}\$',
+                            style: TextStyle(color: ProjectColors.grey3),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Text(
-                previousPrices,
-                style: ProjectTextStyles.sub.copyWith(color: color),
-              ),
-            ],
-          ),
-        ],
+                  ],
+                ),
+                Text(
+                  previousPrices,
+                  style: ProjectTextStyles.sub.copyWith(color: color),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
